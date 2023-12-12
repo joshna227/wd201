78ad2d2fb7423a7268cdb56ff03acccfbf43a2cd
joshna227 <joshnasachu272@gmail.com>
2023-12-12 22:17:07 +0530

    initial commit todo files

---
+++ todo-cli/todo.js
@@ -0,0 +1,136 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-undef */
+const todoList = () => {
+  all = []
+  const add = (todoItem) => {
+    all.push(todoItem)
+  }
+  const markAsComplete = (index) => {
+    all[index].completed = true
+  }
+
+  const overdue = () => {
+    // Write the date check condition here and return the array
+    // of overdue items accordingly.
+    od = []
+    // eslint-disable-next-line no-undef
+    for (let i = 0; i < all.length; i++) {
+      let te = new Date(all[i].dueDate)
+      if (te.getDate() === yesterday) {
+        od.push(all[i])
+      }
+    }
+    return od
+  }
+
+  const dueToday = () => {
+    // Write the date check condition here and return the array
+    // of todo items that are due today accordingly.
+    dt = []
+    for (let i = 0; i < all.length; i++) {
+      let te = new Date(all[i].dueDate)
+      if (te.getDate() === today) {
+        dt.push(all[i])
+      }
+    }
+    return dt
+  }
+
+  const dueLater = () => {
+    // Write the date check condition here and return the array
+    // of todo items that are due later accordingly.
+    dl = []
+    for (let i = 0; i < all.length; i++) {
+      let te = new Date(all[i].dueDate)
+      if (te.getDate() === tomorrow) {
+        dl.push(all[i])
+      }
+    }
+  }
+
+  const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string
+    // as per the format given above.
+    out = []
+    for (i = 0; i < list.length; i++) {
+      let te = new Date(all[i].dueDate)
+      if (te === today) {
+        if (list[i].completed === false) {
+          out.push(`[ ] ${list[i].title}`)
+        } else {
+          out.push(`[X] ${list[i].title}`)
+        }
+      } else {
+        if (list[i].completed === false) {
+          out.push(`[ ] ${list[i].title} ${te}`)
+        } else {
+          out.push(`[X] ${list[i].title} ${te}`)
+        }
+      }
+    }
+    return out.join('\n')
+  }
+
+  return {
+    // eslint-disable-next-line no-undef
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList
+  }
+}
+
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+
+const todos = todoList()
+
+const formattedDate = d => {
+  return d.toISOString().split('T')[0]
+}
+
+// eslint-disable-next-line no-var
+var dateToday = new Date()
+const today = formattedDate(dateToday)
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+)
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+)
+
+todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
+todos.add({ title: 'Pay rent', dueDate: today, completed: true })
+todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
+todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
+todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
+
+console.log('My Todo-list\n')
+
+console.log('Overdue')
+// eslint-disable-next-line no-var
+var overdues = todos.overdue()
+// eslint-disable-next-line no-var
+var formattedOverdues = todos.toDisplayableList(overdues)
+console.log(formattedOverdues)
+console.log('\n')
+
+console.log('Due Today')
+// eslint-disable-next-line prefer-const
+let itemsDueToday = todos.dueToday()
+// eslint-disable-next-line prefer-const
+let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
+console.log(formattedItemsDueToday)
+console.log('\n')
+
+console.log('Due Later')
+// eslint-disable-next-line prefer-const
+let itemsDueLater = todos.dueLater()
+// eslint-disable-next-line prefer-const
+let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
+console.log(formattedItemsDueLater)
+console.log('\n\n')
