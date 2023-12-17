455683922df090290257d71531c9e67d00d4137e
joshna227 <joshnasachu272@gmail.com>
2023-12-17 19:52:13 +0530

    Add lint staged

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,28 +1,78 @@
+/* eslint-disable quotes */
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("TodoList Test Suite", () => {
   beforeAll(() => {
     add({
       title: "Test todo",
       completed: false,
-      dueDate: new Date().toISOString(),
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    add({
+      title: "Test todo yesterday",
+      completed: false,
+      duedate: new Date(new Date().setDate(new Date().getDate() - 1))
+        .toISOString()
+        .split("T")[0],
+    });
+    add({
+      title: "Test todo tomorrow",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
+        .toISOString()
+        .split("T")[0],
     });
   });
   test("Should add new todo", () => {
     const todoItemsCount = all.length;
     add({
-      title: "Test todo",
+      title: "Test todo2",
       completed: false,
-      dueDate: new Date().toISOString(),
+      dueDate: new Date().toISOString().slice(0, 10),
     });
     expect(all.length).toBe(todoItemsCount + 1);
   });
+
   test("Should mark a todo as complete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("Should retrieve overdue items", () => {
+    const todoItemsCount = overdue().length;
+    add({
+      title: "Test todo1 yesterday",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
+        .toISOString()
+        .split("T")[0],
+    });
+    expect(overdue().length).toBe(todoItemsCount + 1);
+  });
+
+  test("Should retrieve dueToday items", () => {
+    const todoItemsCount = dueToday().length;
+    add({
+      title: "Test todo1",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    expect(dueToday().length).toBe(todoItemsCount + 1);
+  });
+
+  test("Should retrieve dueLater items", () => {
+    const todoItemsCount = dueLater().length;
+    add({
+      title: "Test todo1 tomorrow",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
+        .toISOString()
+        .split("T")[0],
+    });
+    expect(dueLater().length).toBe(todoItemsCount + 1);
   });
 });
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -80,3 +80,58 @@ const todoList = () => {
   };
 };
 module.exports = todoList;
+
+/*
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+
+const todos = todoList() */
+
+const formattedDate = (d) => {
+  return d.toISOString().split("T")[0];
+};
+
+// eslint-disable-next-line no-var
+var dateToday = new Date();
+const today = formattedDate(dateToday);
+/*
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+)
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+)
+
+todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
+todos.add({ title: 'Pay rent', dueDate: today, completed: true })
+todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
+todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
+todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
+
+console.log('My Todo-list\n')
+
+console.log('Overdue')
+// eslint-disable-next-line no-var
+var overdues = todos.overdue()
+// eslint-disable-next-line no-var
+var formattedOverdues = todos.toDisplayableList(overdues)
+console.log(formattedOverdues)
+console.log('\n')
+
+console.log('Due Today')
+// eslint-disable-next-line prefer-const
+let itemsDueToday = todos.dueToday()
+// eslint-disable-next-line prefer-const
+let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
+console.log(formattedItemsDueToday)
+console.log('\n')
+
+console.log('Due Later')
+// eslint-disable-next-line prefer-const
+let itemsDueLater = todos.dueLater()
+// eslint-disable-next-line prefer-const
+let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
+console.log(formattedItemsDueLater)
+console.log('\n\n')
+*/
